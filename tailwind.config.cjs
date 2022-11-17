be8df514c09bf13bf4fe64bd7b2ded616ad4f9cd
Artemiy Bal <artemiy.bal@helmes.com>
2022-11-17 04:06:53 +0200

    feat: add basic layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primaryText: '#323857',
+        titleText: '#656565',
+        background: '#F2F2F2',
+        header: '#B6C9E6',
+        headerText: '#2667C9',
+        section: '#E2E4F2'
+      }
+    }
   },
   plugins: []
 };
