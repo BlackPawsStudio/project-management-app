a35803f0f7ba14358347901b6c6ffb7f64bb6b8e
Victor Saraev <vvsar@mail.ru>
2022-12-01 02:14:06 +0300

    fix: fix error in i18next.d.ts

--- i18next.d.ts
+++ i18next.d.ts
@@ -2,7 +2,7 @@ import "react-i18next";
 
 import en from "./public/locales/en/translation.json";
 
-declare module "i18next" {
+declare module "react-i18next" {
   interface CustomTypeOptions {
     defaultNS: "translation";
     resources: {
